299dc78687223d4fbfe0c8708c0119fa5aea40c9
natasha-robarge <robarge.natasha7@gmail.com>
2019-03-15 07:53:44 -0500

    add basic search tests

---
+++ script.test.js
@@ -0,0 +1,28 @@
+const googleSearch = require('./script');
+
+dbMock = [
+  'dog.com',
+  'cheesepuff.com',
+  'disney.com',
+  'dogpictures.com'
+]
+
+describe('googleSearch', () => {
+  it('should search google', () => {
+    // expect(googleSearch('testtest', dbMock)).toEqual([]);
+    // expect(googleSearch('dog'), dbMock).toEqual(['dog.com', 'dogpictures.com']);
+  });
+  
+  it('should work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+  
+  it('should not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  });
+});
+
+it('this is a silly test', () => {
+  expect('hello').toBe('hello');
+});
